41f63bb325f33feed69114a5b5bdbecb2a6d1841
Sadam Hussain <sadamhussain942@gmail.com>
2026-01-21 16:01:50 -0500

    Create Requirements.txt

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI;
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # Import Routers
 from app.routes.issues import router as issues_router
 
@@ -8,6 +8,14 @@ from app.middleware.timer import timer_middleware
 
 # Create FastAPI instance
 app = FastAPI()
+# Add CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Add Middleware
 app.middleware("http")(timer_middleware)
